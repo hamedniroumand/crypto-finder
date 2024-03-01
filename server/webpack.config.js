415af773e1ed956ebb15a99a6d7c93e60a5ddae2
Hamed N <dev.hamedniroomand@gmail.com>
2024-03-02 02:29:38 +0330

    feat: initial server

---
+++ server/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.ts', // Entry point of your application
+  output: {
+    filename: 'bundle.js', // Output bundle name
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // Match TypeScript files
+        use: 'ts-loader', // Use the TypeScript loader
+        exclude: /node_modules/, // Exclude node_modules directory
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    },
+  },
+};
